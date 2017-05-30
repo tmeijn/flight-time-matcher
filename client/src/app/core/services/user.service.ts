39fc80e89df70c9841c9c77e8da4000fe9b54807
Tyrone Meijn <tyrone_meijn@hotmail.com>
2017-05-30 09:43:30 +0200

    Refactor User service and consolidate auth service into user service.

--- client/src/app/core/services/user.service.ts
+++ client/src/app/core/services/user.service.ts
@@ -1,21 +1,67 @@
-import { FeathersRestService } from './feathers.service';
+import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
+import { User } from "app/core/models/user.model";
+
+export const MOCK_USER = new User();
+MOCK_USER._id = "1";
+MOCK_USER.email = "foo@test.com";
+MOCK_USER.firstName = "Foo";
+MOCK_USER.lastName = "Bar";
+MOCK_USER.password = "password";
+
+/**
+ * The user service.
+ */
 
 @Injectable()
 export class UserService {
-    private _user = {};
-    
-    constructor() {
-    }
 
-    set user(value) {
-        this._user = value;
-        console.log('user set:', value);
-    }
+  /**
+   * True if authenticated
+   * @type {boolean}
+   */
+  private _authenticated = false;
 
-    get user() {
-        return this._user;
-    }
+  /**
+   * Authenticate the user
+   * 
+   * @param email the user's email address
+   * @param password the user's password
+   * @returns {Observable<User>} the authenticated user observable
+   */
+  public authenticate(email: string, password: string): Observable<User> {
+    //TODO: implement HTTP auth request.
 
+    if(email === MOCK_USER.email && password === MOCK_USER.password) {
+      this._authenticated = true;
+      return Observable.of(MOCK_USER);
+    }
+  }
+
+  /**
+   * Determines if the user is authenticated
+   * @returns {Observable<boolean>}
+   */
+  public authenticated(): Observable<boolean> {
+    return Observable.of(this._authenticated);
+  }
+
+  /**
+   * Returns the authenticated user
+   * @returns {User}
+   */
+  public authenticatedUser(): Observable<User> {
+    return Observable.of(MOCK_USER);
+  }
+
+  /**
+   * Creates a new user and returns the creation Object
+   * @param {User} user
+   */
+  public create(user: User): Observable<User> {
+    //TODO: Implement HTTP POST.
+    this._authenticated = true;
+    return Observable.of(user);
+  }
 
 }
